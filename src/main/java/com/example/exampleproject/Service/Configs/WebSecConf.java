66708c73e1c98bb66d682eeb478a63f5d618a423
Me <aagamer@yandex.ru>
2022-06-06 13:05:30 +0300

    1

--- src/main/java/com/example/exampleproject/Service/Configs/WebSecConf.java
+++ src/main/java/com/example/exampleproject/Service/Configs/WebSecConf.java
@@ -1,39 +1,50 @@
-//package com.example.exampleproject.Service.Configs;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.access.AccessDeniedHandler;
-//import org.springframework.stereotype.Component;
-//
-//
-//@Component
-//@EnableAutoConfiguration
-//public class WebSecConf extends WebSecurityConfigurerAdapter {
-//    @Autowired
-//    private MyAP aadapter;
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//        auth.authenticationProvider(aadapter);
-//
-//    }
-//
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//
-//        auth.inMemoryAuthentication()
-//                .withUser("user").password("password").roles("USER")
-//                .and()
-//                .withUser("admin").password("password").roles("ADMIN");
-//    }
-//
-//
-//
-//}
+package com.example.exampleproject.Service.Configs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@EnableAutoConfiguration
+public class WebSecConf extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyAP aadapter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.authenticationProvider(aadapter);
+
+    }
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
+                .antMatchers("/products").hasAnyAuthority( "ROLE_USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN");
+    }
+
+
+
+}
