820bc8b1ee5cbac33d7899144b6cf00c30bff297
Kimrayt <kimrayt61@gmail.com>
2022-06-17 21:03:18 +0300

    Added "Add review from" to business and product

--- src/main/java/com/example/exampleproject/controller/BusinessReviewController.java
+++ src/main/java/com/example/exampleproject/controller/BusinessReviewController.java
@@ -1,12 +1,18 @@
 package com.example.exampleproject.controller;
 
 import com.example.exampleproject.model.BusinessReview;
+import com.example.exampleproject.model.ProductReview;
 import com.example.exampleproject.repository.BusinessReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -25,4 +31,22 @@ public class BusinessReviewController {
         model.addAttribute("businessReviews", businessReviews);
         return "businessReview";
     }
+
+    @RequestMapping(value = "/business/{id}/add-review", method = RequestMethod.POST)
+    public String addReview(@PathVariable("id") int id, Model model, BindingResult result, @Valid BusinessReview br) {
+        businessReviewRepository.save(br);
+        return "redirect:/business" + id;
+    }
+    @GetMapping("/business/{id}/update-review")
+    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+        BusinessReview br = businessReviewRepository.findById(id).get();
+        model.addAttribute("productReview", br);
+        return "redirect:/business" + id;
+    }
+    @RequestMapping(value = "/business/{id}/edit-review", method = RequestMethod.POST)
+    public String editReview(@PathVariable("id") int id, Model model, BindingResult result, @Valid BusinessReview br) {
+        businessReviewRepository.save(br);
+        return "redirect:/business" + id;
+    }
+
 }
